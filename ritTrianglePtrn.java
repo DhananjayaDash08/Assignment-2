afb94da129473ce5091cdae6435fe859fa1a49f3
DhananjayaDash08 <94070065+DhananjayaDash08@users.noreply.github.com>
2021-12-15 11:52:06 +0530

    Add files via upload

---
+++ ritTrianglePtrn.java
@@ -0,0 +1,23 @@
+package Geekster_Test;
+
+import java.util.Scanner;
+
+public class ritTrianglePtrn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num:");
+		int num= sc.nextInt();
+		int i, j;
+        for(i=1; i<=num; i++)
+        {
+        	for(j=1; j<=i; j++)
+        	{
+        		System.out.print('*');
+        	}
+        	System.out.println();
+        }
+	}
+
+}
